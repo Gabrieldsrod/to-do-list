7d7e5cba51883702b29de6a4913165d139a28c03
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2025-11-07 22:10:28 -0300

    Adiciona configuração do banco de dados H2 e testes para criação de Todo

--- todo-list-backend/src/test/java/br/com/gabrieldsrodrigues/todo_list/TodoListApplicationTests.java
+++ todo-list-backend/src/test/java/br/com/gabrieldsrodrigues/todo_list/TodoListApplicationTests.java
@@ -1,13 +1,51 @@
 package br.com.gabrieldsrodrigues.todo_list;
 
+import br.com.gabrieldsrodrigues.todo_list.domain.entity.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+
+import static br.com.gabrieldsrodrigues.todo_list.domain.enums.TodoPriority.MEDIUM;
+import static br.com.gabrieldsrodrigues.todo_list.domain.enums.TodoStatus.ONGOING;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoListApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", ONGOING, MEDIUM);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].title").isEqualTo(todo.getTitle())
+				.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+				.jsonPath("$[0].status").isEqualTo(todo.getStatus().getStatus())
+				.jsonPath("$[0].priority").isEqualTo(todo.getPriority().getValue());
 	}
+
+	@Test
+	void testCreateTodoFail() {
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(
+						new Todo("", "", null, null)
+				).exchange()
+				.expectStatus().isBadRequest();
+
+	}
+
 
 }
